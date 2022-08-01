3161731794d6e786625c16806284348478b15139
Adam Slay <adamslay11@gmail.com>
2022-08-01 17:12:00 -0500

    update Playfair

--- playfair/playfair.py
+++ playfair/playfair.py
@@ -3,7 +3,9 @@
 def playfair(strng, key, encode=True):
     AZ = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     diagram = []
-    d = []
+    row = []
+
+    # -----Diagram-----
 
     # Convert key to upper + no space
     key = key.upper()
@@ -13,31 +15,77 @@ def playfair(strng, key, encode=True):
     for letter in key:
         if letter == 'J': letter = 'I'
 
-        if len(d) < 5 and letter in AZ:
-            d.append(letter)
+        if len(row) < 5 and letter in AZ:
+            row.append(letter)
             AZ = AZ.replace(letter, '')
         elif letter in AZ:
-            diagram.append(d)
-            d = []
-            d.append(letter)
+            diagram.append(row)
+            row = []
+            row.append(letter)
             AZ = AZ.replace(letter, '')
 
     # Fill in rest of diagram using remaining AZ
     for letter in AZ:
         if letter == 'J': letter = 'I'
 
-        if len(d) < 5 and letter in AZ:
-            d.append(letter)
+        if len(row) < 5 and letter in AZ:
+            row.append(letter)
             AZ = AZ.replace(letter, '')
         elif letter in AZ:
-            diagram.append(d)
-            d = []
-            d.append(letter)
+            diagram.append(row)
+            row = []
+            row.append(letter)
             AZ = AZ.replace(letter, '')
     # Append last row to diagram
-    diagram.append(d)
+    diagram.append(row)
 
-    return diagram
+    # -----Format Strng----- 
+
+    # Convert strng to upper + no space
+    strng = strng.upper()
+    strng = strng.replace(' ', '')
+
+    # Split strng into pairs
+    # If pair is two of the same letter,
+    # Add 'X' between them
+    pair = []
+    plain_text = []
+    for letter in strng:
+        if len(pair) == 1:
+            if pair[0] == letter:
+                pair.append('X')
+                plain_text.append(pair)
+                pair = [letter]
+            else:
+                pair.append(letter)
+                plain_text.append(pair)
+                pair = []
+        else:
+            pair.append(letter)
+    # Check if there is a leftover letter in pair.
+    # If so, add 'X' and append to plain_text
+    if len(pair) == 1:
+        pair.append('X')
+        plain_text.append(pair)
+
+
+    # -----Encode-----
+    if encode:
+        encoded = []
+        for pair in plain_text:
+            coord_a = []
+            coord_b = []
+            for row in diagram:
+                if pair[0] in row:
+                    coord_a.append(diagram.index(row))
+                    coord_a.append(diagram[diagram.index(row)].index(pair[0]))
+                if pair[1] in row:
+                    coord_b.append(diagram.index(row))
+                    coord_b.append(diagram[diagram.index(row)].index(pair[1]))
+            return coord_a, coord_b
+
+
+
 
 print(playfair('hello', 'quiet'))
 
